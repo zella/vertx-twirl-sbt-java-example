206a6b7c672915ee5f83d68aa4d450020d159e9d
zella <drumirage@gmail.com>
2017-03-17 12:53:54 +0700

    apply to render - java api

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
     // Convenience method so you can run it in your IDE
     public static void main(String[] args) {
         Vertx.vertx().createHttpServer().requestHandler(req -> {
-              req.response().end(html.simple.apply("Zella", Arrays.asList("One", "Two", "Three")).body());
+              req.response().end(html.simple.render("Zella", Arrays.asList("One", "Two", "Three")).body());
           }
         ).listen(8080);
         System.out.println("Running http server on 8080...");
